9aad96bc664f0a6ca8479321654c7b4e0bcf8081
Unknown <dustin.harris@outlook.com>
2018-04-22 10:10:08 -0700

    Many UI changes

---
+++ Assets/Scripts/EquipmentButtonBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentButtonBehavior : MonoBehaviour {
+
+    [SerializeField] private GameObject activeObjectsEmpty;
+    [SerializeField] private GameObject equipmentObject;
+
+	public void EquipmentButtonPressed()
+    {
+        // Parent object to active objects
+        equipmentObject.transform.SetParent(activeObjectsEmpty.transform);
+
+        // Make object visible
+        equipmentObject.SetActive(true);
+
+
+        // Set object y to 0 (ground)
+        float newX = equipmentObject.transform.position.x;
+        float newZ = equipmentObject.transform.position.z;
+
+        equipmentObject.transform.position = new Vector3(newX, 0, newZ);
+    }
+}
